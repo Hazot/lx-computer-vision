e618b5a127cd2c2e144912b1c44a893b77a2b14b
Kevin Lessard <kevinlessard74@gmail.com>
2024-11-05 03:42:21 -0500

    trying different values for controlling stuff

--- packages/visual_lane_servoing/include/visual_servoing_solution.py
+++ packages/visual_lane_servoing/include/visual_servoing_solution.py
@@ -16,12 +16,17 @@ def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
 
     # TODO: implement your own solution here
     steer_matrix_left = np.zeros(shape)
-    h, w = shape
 
-    # Create a gradient matrix that gives higher weights to left side markings
-    for i in range(h):
-        for j in range(w // 2):  # Left half of the image
-            steer_matrix_left[i, j] = (w // 2 - j) / (w // 2)
+    half = shape[1] // 2
+    # Sol1
+    # steer_matrix_left[:, :half] = -0.3
+    # steer_matrix_left[: half // 4 + 1, :half] = 0.1
+
+    # Sol2
+    steer_matrix_left[int(shape[0] * 5 / 8) :, :half] = -0.01
+
+    # Sol3
+    steer_matrix_left[:, shape[1] // 3 :] = -1
 
     return steer_matrix_left
 
@@ -38,12 +43,17 @@ def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
 
     # TODO: implement your own solution here
     steer_matrix_right = np.zeros(shape)
-    h, w = shape
 
-    # Create a gradient matrix that gives higher weights to right side markings
-    for i in range(h):
-        for j in range(w // 2, w):  # Right half of the image
-            steer_matrix_right[i, j] = (j - w // 2) / (w // 2)
+    # Sol1
+    half = shape[1] // 2
+    # steer_matrix_right[half // 4 + 1 :, half:] = 0.2
+    # steer_matrix_right[: half // 4 + 1, half:] = 0.1
+
+    # Sol2
+    # steer_matrix_right[int(shape[0] * 5 / 8) :, half:] = 0.01
+
+    # Sol3
+    steer_matrix_right[:, : shape[1] * 2 // 3] = 1
 
     return steer_matrix_right
 
@@ -56,43 +66,38 @@ def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         mask_left_edge:   Masked image for the dashed-yellow line (numpy.ndarray)
         mask_right_edge:  Masked image for the solid-white line (numpy.ndarray)
     """
+    # TODO: implement your own solution here
     height, width, _ = image.shape
 
-    imgbgr = image
-
-    # OpenCV uses BGR by default, whereas matplotlib uses RGB, so we generate an RGB version for 
-    # the sake of visualization
-    imgrgb = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2RGB)
-
-    # Convert the image to HSV for any color-based filtering
-    imghsv = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2HSV)
-
-    # Most of our operations will be performed on the grayscale version
-    img = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2GRAY)
-
-    # TODO: implement your own solution here
-    # Parameters to play with
+    # Parameters to play around with
     sigma_gaussian_blur = 4
     white_lower_hsv = np.array([0, 0, 120])
     white_upper_hsv = np.array([180, 75, 255])
     yellow_lower_hsv = np.array([15, 60, 60])
     yellow_upper_hsv = np.array([45, 255, 255])
 
+    # 0) Processing the image
+    imgbgr = image
+    # OpenCV uses BGR by default, whereas matplotlib uses RGB
+    # Convert the image to HSV for any color-based filtering
+    imghsv = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2HSV)
+    # Most of our operations will be performed on the grayscale version
+    img = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2GRAY)
+
     # 1) (Optional) Find the horizon and remove it
-    mask_ground = np.ones((height, width), dtype=np.uint8)
+    # mask_ground = np.ones((height, width), dtype=np.uint8)
     # mask_ground[:175, :] = 0
 
     # 2) Smooth the image using a Gaussian kernel
     img_gaussian_filter = cv2.GaussianBlur(img, (0, 0), sigma_gaussian_blur)
 
     # 3) Convolve the image with the Sobel operator (filter) to compute the
-    # numerical derivatives in the x and y directions with gaussian filter
     sobelx = cv2.Sobel(img_gaussian_filter, cv2.CV_64F, 1, 0)
     sobely = cv2.Sobel(img_gaussian_filter, cv2.CV_64F, 0, 1)
     Gmag = np.sqrt(sobelx * sobelx + sobely * sobely)  # magnitude of gradients
 
     # 4) Make a mask to clip filter out weaker gradients (form of non-maximal suppression)
-    threshold = np.max(Gmag) // 5  # CHANGE ME
+    threshold = np.max(Gmag) // 5
     mask_mag = Gmag > threshold
 
     # 5) Let's create masks for the left and right halves of the image
@@ -102,10 +107,6 @@ def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     mask_right[:, 0 : int(np.floor(width / 2))] = 0
 
     # 6) Generate a mask that identifies pixels based on the sign of their x-derivative
-    # In the left-half image, we are interested in the right-half of the dashed yellow line,
-    # which corresponds to negative x- and y-derivatives
-    # In the right-half image, we are interested in the left-half of the solid white line,
-    # which correspons to a positive x-derivative and a negative y-derivative
     mask_sobelx_pos = sobelx > 0
     mask_sobelx_neg = sobelx < 0
     mask_sobely_pos = sobely > 0
@@ -117,20 +118,10 @@ def detect_lane_markings(image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
 
     # Let's generate the complete set of masks, including those based on color
     mask_left_edge = (
-        mask_ground
-        * mask_left
-        * mask_mag
-        * mask_sobelx_neg
-        * mask_sobely_neg
-        * mask_yellow
+        mask_left * mask_mag * mask_sobelx_neg * mask_sobely_neg * mask_yellow
     )
     mask_right_edge = (
-        mask_ground
-        * mask_right
-        * mask_mag
-        * mask_sobelx_pos
-        * mask_sobely_neg
-        * mask_white
+        mask_right * mask_mag * mask_sobelx_pos * mask_sobely_neg * mask_white
     )
 
     return mask_left_edge, mask_right_edge
